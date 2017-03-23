f38999ed1ca0107eb1a8a322a5b4e4ae5f149dfc
Zowie van Geest <zowie@zowievangeest.nl>
2017-03-23 20:50:12 +0100

    Added routing

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {RoutingModule} from "./modules/routing.module";
+
+import { AppComponent } from './app/app.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RoutingModule,
+
+    RouterModule,
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
