c9d07e81bfda98c83e542c96d4698a5afec48cc5
Bas van Bovene <bas@bovene.nl>
2017-04-04 10:45:50 +0200

    tables controllers models added

---
+++ app/Http/Controllers/StatisticController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ipmedt5c\Http\Controllers;
+
+use ipmedt5c\statistic;
+use Illuminate\Http\Request;
+
+class StatisticController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \ipmedt5c\statistic  $statistics
+     * @return \Illuminate\Http\Response
+     */
+    public function show(statistic $statistics)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \ipmedt5c\statistic  $statistics
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(statistic $statistics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \ipmedt5c\statistic  $statistics
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, statistic $statistics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \ipmedt5c\statistic  $statistics
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(statistic $statistics)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_04_03_193202_statistics_views.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class StatisticsViews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics_views', function (Blueprint $table) {
+            $table->increments('statistic_id');
+            $table->increments('View_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
