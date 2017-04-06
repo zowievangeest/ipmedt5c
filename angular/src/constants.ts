6191ed2026555fbf218d92a141b69ee9cdb98dc4
Zowie van Geest <zowie@zowievangeest.nl>
2017-04-06 13:59:25 +0200

    Build

--- angular/src/constants.ts
+++ angular/src/constants.ts
@@ -1,2 +1,2 @@
-export const url = 'http://ipmedt5c.dev/api/';
-// export const url = 'https://iot.zowie.online/api/';
+// export const url = 'http://ipmedt5c.dev/api/';
+export const url = 'https://iot.zowie.online/api/';
